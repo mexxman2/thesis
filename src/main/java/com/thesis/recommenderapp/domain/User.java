43fde7956c0ee36d91b779537e487b933abc7991
Dominik Ziman <dominik_ziman@epam.com>
2019-11-07 22:42:11 +0100

    user table renamed

--- src/main/java/com/thesis/recommenderapp/domain/User.java
+++ src/main/java/com/thesis/recommenderapp/domain/User.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "reco_user")
 @Data
 public class User {
 
