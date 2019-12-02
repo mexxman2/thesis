a40d7d1fe258b9f0eeefabfdbbb090f5ff9c7a02
Dominik Ziman <dominik_ziman@epam.com>
2019-12-02 13:12:33 +0100

    Repo change

--- src/main/java/com/thesis/recommenderapp/dao/UserDao.java
+++ src/main/java/com/thesis/recommenderapp/dao/UserDao.java
@@ -2,11 +2,11 @@ package com.thesis.recommenderapp.dao;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thesis.recommenderapp.domain.User;
 
-public interface UserDao extends CrudRepository<User, Long> {
+public interface UserDao extends JpaRepository<User, Long> {
 
     Page<User> findAllByUserNameContainingIgnoreCase(String substring, Pageable pageable);
 
--- src/main/java/com/thesis/recommenderapp/dao/WatchedDao.java
+++ src/main/java/com/thesis/recommenderapp/dao/WatchedDao.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thesis.recommenderapp.domain.Item;
 import com.thesis.recommenderapp.domain.User;
 import com.thesis.recommenderapp.domain.Watched;
 
-public interface WatchedDao extends CrudRepository<Watched, Long> {
+public interface WatchedDao extends JpaRepository<Watched, Long> {
 
     Page<Watched> findAllByUserId(Long userId, Pageable pageable);
 
