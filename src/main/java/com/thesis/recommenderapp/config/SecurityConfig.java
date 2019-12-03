ba3e52e1853fec1424163dc50626b23581792ce5
Dominik Ziman <dominik_ziman@epam.com>
2019-12-03 14:04:39 +0100

    Refactor

--- src/main/java/com/thesis/recommenderapp/config/SecurityConfig.java
+++ src/main/java/com/thesis/recommenderapp/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/index", "/", "/register", "/registerUserPost")
+                .antMatchers("/index", "/", "/register", "/registerUserPost", "/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
